16bb2d8f4962127e17a688f0e70ea9a64f690eea
Calvin <calvinalvito19@gmail.com>
2024-08-13 23:12:53 +0700

    chore: add knexfile

---
+++ knexfile.ts
@@ -0,0 +1,52 @@
+import dotenv from 'dotenv';
+import { Knex } from 'knex';
+
+// Load environment variables from .env file
+dotenv.config();
+
+const knexConfig: { [key: string]: Knex.Config } = {
+  development: {
+    client: 'pg',
+    connection: {
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      directory: './migrations',
+      tableName: 'knex_migrations',
+    },
+    seeds: {
+      directory: './seeds',
+    },
+  },
+
+  production: {
+    client: 'pg',
+    connection: {
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      directory: './migrations',
+      tableName: 'knex_migrations',
+    },
+    seeds: {
+      directory: './seeds',
+    },
+  }
+};
+
+export default knexConfig;
