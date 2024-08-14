d1efa9c995ec60fb1e7dfe2500bfa45e371acb97
Calvin <calvinalvito19@gmail.com>
2024-08-14 14:00:50 +0700

    fix server

--- src/server.ts
+++ src/server.ts
@@ -11,12 +11,28 @@ const port = process.env.PORT || 3000;
 
 const db = knex(knexConfig.development);
 
+// Middleware
 app.use(cors());
 app.use(json());
 
+// Routes
 app.use('/api', userRouter);
 app.use('/api', taskRoutes);
 
+// Check database connection
+const checkDatabaseConnection = async () => {
+  try {
+    await db.raw('SELECT 1+1 as result');
+    console.log('==========Database Connected Successfully==========');
+  } catch (error: unknown) {
+    console.error('Error Connecting to Database:', (error as Error).message);
+  }
+};
+checkDatabaseConnection();
+
+// Start server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+export default app;
