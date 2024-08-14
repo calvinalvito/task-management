e485cf87bf0721c3138738ae7813c441d29a4757
Calvin <calvinalvito19@gmail.com>
2024-08-14 14:29:23 +0700

    fix server

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
-import express from 'express';
-import cors from 'cors';
-import { json } from 'body-parser';
-import knex from 'knex';
-import knexConfig from '../knexfile';
-import userRouter from './routes/userRoutes';
-import taskRoutes from './routes/taskRoutes';
+import express from "express";
+import cors from "cors";
+import { json } from "body-parser";
+import knex from "knex";
+import knexConfig from "../knexfile";
+import userRouter from "./routes/userRoutes";
+import taskRoutes from "./routes/taskRoutes";
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -16,16 +16,16 @@ app.use(cors());
 app.use(json());
 
 // Routes
-app.use('/api', userRouter);
-app.use('/api', taskRoutes);
+app.use("/api", userRouter);
+app.use("/api", taskRoutes);
 
 // Check database connection
 const checkDatabaseConnection = async () => {
   try {
-    await db.raw('SELECT 1+1 as result');
-    console.log('==========Database Connected Successfully==========');
+    await db.raw("SELECT 1+1 as result");
+    console.log("==========Database Connected Successfully==========");
   } catch (error: unknown) {
-    console.error('Error Connecting to Database:', (error as Error).message);
+    console.error("Error Connecting to Database:", (error as Error).message);
   }
 };
 checkDatabaseConnection();
